24f6f1f7e33fdf6891f47e7e26248a0ea7726952
Ivan Grech <Ivan Grech>
2017-12-14 19:07:36 +0100

    Pong2D_v0.4 Lose Colliders added aswell as LevelManager Script

---
+++ Pong2D_0.4/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string levelName)
+    {
+        Debug.Log("Level: " + levelName);
+        //loads the scene named levelName
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /*public void BrickDestroyed()
+    {
+        //when all bricks are destroyed load next level
+        if (Bricks.breakableCount <= 0)
+        {
+            LoadNextLevel();
+        }
+    }
+    */
+}
