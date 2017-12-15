89fe0ef858de47f024f07897a8ef67b3d2cbfc21
Ivan Grech <Ivan Grech>
2017-12-15 17:27:39 +0100

    Pong2D_v0.7 Pushed everything from Level_01 to Prefabs and started Level_02

---
+++ Pong2D_0.7/Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxSpeed;
+
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        maxSpeed = 4;
+
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveVertical();
+    }
+
+    void MoveVertical()
+    {
+        transform.position = new Vector3(transform.position.x, startPosition.y + Mathf.PingPong(maxSpeed, startPosition.y), transform.position.z);
+
+        if (transform.position.y > 9.0f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.y < -9.0f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Pong2D_0.7/Assets/Scripts/Player2Controls.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controls : MonoBehaviour {
+
+    public float boundY = 3.2f;
+
+    private Vector3 offset;
+
+    float xPos;
+
+    void Awake()
+    {
+        xPos = transform.position.x;
+    }
+
+    void OnMouseDown()
+    {
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(xPos, Input.mousePosition.y));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector2 newPosition = new Vector2(xPos, Input.mousePosition.y);
+        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+    }
+
+    void Update()
+    {
+        var pos = transform.position;
+        if (pos.y > boundY)
+        {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY)
+        {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
+    }
+}
