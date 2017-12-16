3368d116743ab687b96493acc3239809275fb69a
Ivan Grech <Ivan Grech>
2017-12-16 20:52:09 +0100

    Pong2D_v0.9 Added sounds in game and the final Scene (Win)

---
+++ Pong2D_0.9/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        Debug.Log("Level: " + levelName);
+        //loads the scene named levelName
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ScoreLimit()
+    {
+        //when score == 7 load next level
+        if (Score.counter == 2)
+        {
+            LoadNextLevel();
+        }
+    }
+}
---
+++ Pong2D_0.9/Assets/Scripts/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    public static int counter = 0;
+    public GUIText player1ScoreLabel, player2ScoreLabel;
+    private static int player1Score, player2Score;
+
+    public static void WallHit(bool leftWall)
+    {
+        if (leftWall)
+        {
+            player2Score++;
+            counter++;
+        }
+        else
+        {
+            player1Score++;
+            counter++;
+        }
+    }
+
+    void OnGUI()
+    {
+        player1ScoreLabel.text = "" + player1Score;
+        player2ScoreLabel.text = "" + player2Score;
+    }
+}
