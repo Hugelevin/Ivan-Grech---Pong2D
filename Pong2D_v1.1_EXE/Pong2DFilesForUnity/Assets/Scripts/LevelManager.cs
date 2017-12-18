20ccdf303d8c1311f78ae8eea856f86525394e85
Ivan Grech <Ivan Grech>
2017-12-18 17:44:44 +0100

    This is the exe for the Pong2D FINAL Game version 1.1

---
+++ Pong2D_v1.1_EXE/Pong2DFilesForUnity/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        Debug.Log("Level: " + levelName);
+        //loads the scene named levelName
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+
+
+    public static void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Pong2D_v1.1_EXE/Pong2DFilesForUnity/Assets/Scripts/Score.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour
+{
+    public GUIText player1ScoreLabel, player2ScoreLabel;
+    public static int player1Score, player2Score;
+
+    void Start()
+    {
+        player1Score = 0;
+        player2Score = 0;
+    }   
+
+
+    public static void WallHit(bool leftWall)
+    {
+        if (leftWall)
+        {
+            player2Score++;
+        }
+        else
+        {
+            player1Score++;
+        }
+    }
+
+    public void OnGUI()
+    {
+        player1ScoreLabel.text = "" + player1Score;
+        player2ScoreLabel.text = "" + player2Score;
+    }
+
+    void Update()
+    {
+
+        if (player1Score == 4 || player2Score == 4)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
