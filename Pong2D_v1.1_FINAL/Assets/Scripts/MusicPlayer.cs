75d1bf1ae7d82263f73068d16be9a40de293120a
Ivan Grech <Ivan Grech>
2017-12-18 17:42:37 +0100

    Pong2D_v1.1FINAL More major improvements and bug fixes.

---
+++ Pong2D_v1.1_FINAL/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    // Static makes use of the same instance for the same class
+    static MusicPlayer myMusicPlayer = null;
+
+    void Awake()
+    {
+
+        if (myMusicPlayer != null)
+        {
+            Debug.Log("Destroying Object: " + this.gameObject.GetInstanceID().ToString());
+            //Destroys the new object created
+            Destroy(this.gameObject);
+        }
+        else //myMusicPlayer = null
+        {
+            //starts the music again
+            myMusicPlayer = this;
+        }
+        // this.gameObject is attached to the script(SoundObject)
+        // DontDestroyOnLoad doesn't destroy the object when loading a new scene
+        GameObject.DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Pong2D_v1.1_FINAL/Assets/Scripts/Player1Controls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controls : MonoBehaviour
+{
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public float velocity = 15.0f;
+    public float boundY = 3.2f;
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int mult = 0;
+        if (Input.GetKey(moveUp))
+        {
+            mult += 1;
+        }
+        if (Input.GetKey(moveDown))
+        {
+            mult -= 1;
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, mult * velocity);
+
+        var pos = transform.position;
+        if (pos.y > boundY)
+        {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY)
+        {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
+    }
+}
